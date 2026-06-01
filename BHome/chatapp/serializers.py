872c5bdaa7ccf42664be160b1fb8530bd4436ff2
Vedant2402 <vedantkankate22@gmail.com>
2026-05-31 23:57:31 -0700

    fixes in serializers.py & changes in view.py

--- BHome/chatapp/serializers.py
+++ BHome/chatapp/serializers.py
@@ -11,3 +11,45 @@ class UserSerializer(serializers.ModelSerializer):
             user = User.objects.create_user(**validated_data)
             return user
         
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
+        
+        
+class ConversationSerializer(serializers.ModelSerializer):
+    participants = UserListSerializer(many=True, read_only=True)
+    
+    class Meta:
+        model = Conversation
+        fields = ['id', 'participants', 'created_at']
+        
+        def to_representation(self, instance):
+            representation = super().to_representation(instance)
+            return representation
+        
+
+class MessageSerializer(serializers.ModelSerializer):
+    sender = UserListSerializer(read_only=True)
+    participants = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = Message
+        fields = ['id', 'conversation', 'sender', 'content', 'timestamp']  
+        
+        def to_representation(self, obj):
+            return UserlistSerializer(obj.conversation.participants.all(), many=True).data
+        
+class CreateMessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = ['conversation', 'content']
+        
+class Message(models.Model):
+    conversation = models.ForeignKey(Conversation, related_name='messages', on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.sender.username} in {self.content[:20]} at {self.timestamp}"
--- BHome/chatapp/views.py
+++ BHome/chatapp/views.py
@@ -1,3 +1,54 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from .models import *
+from .serializers import *
+from rest_framework.exceptions import PermissionDenied, NotFound
 
 # Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# listing of users
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserListSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    
+#creating, listing and viewing conversations
+
+class ConversationListCrateView(generics.ListCreateAPIView):
+    serializer_class = ConversationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_queryset(self):
+        return (conversation.objects.filter(participants=self.request.user).prefetch_related('participants'))
+    
+    def create(self, request, *args, **kwargs):
+        participants_ids = request.data.get('participants', [])
+        
+        if len(participants_ids) != 2:
+            return Response({'error': 'At least two participants are required to create a conversation.'}, 
+                            status=status.HTTP_400_BAD_REQUEST)
+            
+        if str(request.user.id) not in participants_ids:
+            return Response(
+                {'error': 'You must be a participant in the conversation.'}, 
+                            status=status.HTTP_403_FORBIDDEN)
+            
+        users = User.objects.filter(id__in=participants_data)
+        if users.count() != 2:
+            return Response({'error': 'One or more participants not found.'}, 
+                            status=status.HTTP_400_BAD_REQUEST)
+            
+        existing_conversation = Conversation.objects.filter(
+            participants__id=participants_data[0]
+            ).filter(
+                participants__id=participants_data[1]
+            ).distinct()
